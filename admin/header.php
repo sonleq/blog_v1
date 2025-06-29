8f42b67363d72d64b6a77391521993f96c57f8a7
sonleq <67938287+sonleq@users.noreply.github.com>
2025-06-28 21:14:16 -0400

    Create header.php

---
+++ admin/header.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-default top-navbar" role="navigation">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="../index.php" target="_blank">Son Le</a>
+            </div>
+        </nav>
+        <!--/. NAV TOP  -->
+  
